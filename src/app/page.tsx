6365e7a8616a37f908b3e980aee54ced9c877a48
Gabriel-otirB <lordglayder@gmail.com>
2025-03-20 11:19:26 -0300

    Implemented Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <h1>Teste página</h1>
+
     </main>
   );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,40 @@
+"use client"
+
+import { useState, useEffect, useCallback } from 'react';
 import styles from './styles.module.scss';
+import Link from 'next/link';
 
 const Header = () => {
+  const [top, setTop] = useState(true);
+
+  const scrollHandler = useCallback(() => {
+    window.scrollY > 10 ? setTop(false) : setTop(true);
+  }, []);
+
+  useEffect(() => {
+
+    window.addEventListener("scroll", scrollHandler);
+
+  }, []);
+
   return (
-    <h1>Header</h1>
+    <header className={`${styles.header} ${!top ? styles.fixed : styles.background}`}>
+      <div className={styles.container}>
+        <div className={styles.content}>
+
+          <div className={styles.contentLogo}>
+            <Link href="/">Dev Motors</Link>
+          </div>
+
+          <nav className={styles.nav}>
+            <Link href={"/"}>HOME</Link>
+            <Link href={"/#servicos"}>SERVIÇOS</Link>
+            <Link href={"/#contatos"}>CONTATOS</Link>
+          </nav>
+
+        </div>
+      </div>
+    </header>
   );
 }
 
